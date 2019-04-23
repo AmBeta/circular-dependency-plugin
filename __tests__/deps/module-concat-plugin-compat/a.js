e2465ffb7aed98cdbac264b4b5242811854cb4f2
AmBeta <cnxcchenyao@gmail.com>
2019-04-23 09:33:48 +0800

    add support for allowAsyncCycles option for version 3

--- __tests__/deps/module-concat-plugin-compat/a.js
+++
@@ -1,5 +0,0 @@
-import b from './b'
-
-var a = { name: 'a', dep: b && b.name }
-
-export default a
--- __tests__/deps/module-concat-plugin-compat/b.js
+++
@@ -1,5 +0,0 @@
-import a from './a'
-
-var b = { name: 'b', dep: a && a.name }
-
-export default b
